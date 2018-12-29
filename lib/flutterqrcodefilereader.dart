43dca23cca91a99fb8dce66c4317b0ed1ce729bd
changfeifan <283410998@qq.com>
2018-12-29 18:31:32 +0800

    [feat] some base file.

---
+++ lib/flutterqrcodefilereader.dart
@@ -0,0 +1,14 @@
+library flutterqrcodefilereader;
+
+import 'dart:io';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  String calculateQrCodeFile(File imageFile) {
+
+    return '';
+  }
+}
